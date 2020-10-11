c44290c0383104adc0677807539f6da1625a4a0b
Oluxmw <72694020+Oluxmw@users.noreply.github.com>
2020-10-11 14:34:48 +0200

    Changed weather location to Nîmes :)

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '42e49ea15846071143e5a02b02520ad9';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 43.834591;
+    let longitude = -4.360860;
 
     getWeather(latitude, longitude);
 }
